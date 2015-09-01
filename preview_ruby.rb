cb45504947f6c652a2a5607f34a0b5236c586ff8
Steph Thirion <phest.contrib@gmail.com>
2015-08-31 19:25:09 -0700

    improved colors for Ruby

---
+++ preview_ruby.rb
@@ -0,0 +1,15 @@
+
+module MyModule
+  class MyClass
+
+    def method_name(arg)
+      :symbol
+      val = 10
+      string = "my val is #{val}"
+      regexp = /hello#{there}/
+      @var = [1,2,3]
+      p(@var)
+    end
+
+  end
+end
